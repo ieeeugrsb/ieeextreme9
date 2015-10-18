2fa1ad70646997b1130f7d5d0ee34b9d0ddd77bb
Benito Palacios Sánchez <benito356@gmail.com>
2015-10-18 11:03:43 +0200

    Add CSharp base tests

---
+++ TestProblem/CSharp/UnitTests/Test.cs
@@ -0,0 +1,45 @@
+﻿//
+//  Test.cs
+//
+//  Author:
+//       Benito Palacios Sánchez (aka pleonex) <benito356@gmail.com>
+//
+//  Copyright (c) 2015 Benito Palacios Sánchez (c) 2015
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using NUnit.Framework;
+using System;
+using System.Collections;
+
+namespace UnitTests
+{
+    [TestFixture()]
+    public class SolutionTests
+    {
+        [Test, TestCaseSource("PublicCases")]
+        public string TestPublicCases(string input)
+        {
+            return "";
+        }
+
+        public IEnumerable PublicCases
+        {
+            get
+            {
+                yield return new TestCaseData("2\nHello\nWorld").Returns("Hello.World");
+            }
+        }
+    }
+}
+
