c080ce4441d1c4b412ede51843429fef0566b986
Israel Blancas <iblancasa@gmail.com>
2015-10-24 21:14:14 +0200

    Pasa los dos primeros test

---
+++ CraftCoders/Xtreme Driving/otro.py
@@ -0,0 +1,80 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+
+def main():
+
+    carretera = []
+    cadena = input().split(" ")
+    largo = int(cadena[0])
+    vacas = int(cadena[1])
+    i =0
+
+
+    if not 2<= largo:
+        print(0)
+        return
+
+    if not largo<=1e18:
+        print(0)
+        return
+
+    if not 0<=vacas:
+        print(0)
+        return
+
+    if not vacas<=100:
+        print(0)
+        return
+
+    if not vacas<=4*(largo-2):
+        print(0)
+        return
+
+
+
+    while i<4:
+        carretera.append([0]*largo)
+        i+=1
+
+    i = 0
+
+    while(i<vacas):
+        cadena = input().split(" ")
+        x = int(cadena[0])
+        y = int(cadena[1])
+        i+=1
+        carretera[x-1][y-1]=99
+
+    carretera[0][0]=1
+
+    i = 0
+    k = 0
+
+    while i<largo-1:
+        if carretera[0][i+1]==99:
+            carretera[0][i+1]=0
+        else:
+            carretera[0][i+1]=carretera[0][i]+carretera[1][i]
+
+        if carretera[1][i+1]==99:
+            carretera[1][i+1]=0
+        else:
+            carretera[1][i+1]=carretera[0][i]+carretera[1][i]+carretera[2][i]
+
+        if carretera[2][i+1]==99:
+            carretera[2][i+1]=0
+        else:
+            carretera[2][i+1]=carretera[1][i]+carretera[2][i]+carretera[3][i]
+
+        if carretera[3][i+1]==99:
+            carretera[3][i+1]=0
+        else:
+            carretera[3][i+1]=carretera[3][i]+carretera[2][i]
+        i+=1
+
+    print(int(carretera[0][largo-1]%(1e9+7)))
+
+
+if __name__ == '__main__':
+    main()
